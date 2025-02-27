b30c62b69fa74a1fc215cd9732fe2800015016b2
Ujjwal <Ujjwalrawat248@gmail.com>
2025-02-27 21:37:38 +0530

    [Author:Ujjwal] ADD:SamMarks.java

---
+++ SamMarks.java
@@ -0,0 +1,9 @@
+class SamMarks{
+	public static void main(String [] args){
+		int maths=94;
+		int physics=95;
+		int chemestry=96;
+		float result=(maths+physics+chemestry)/3;
+		System.out.println("Sam\'s average marks in PCM is "+result);
+	}
+	}
